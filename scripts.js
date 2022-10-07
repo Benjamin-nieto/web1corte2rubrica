356dd5f62e3410dc117ce6c0bd416fad21d65a38
Benjamin nieto <benjamin.nieto@aspsols.com>
2022-10-07 14:10:40 -0500

    validacion de punto fuck punto

--- scripts.js
+++ scripts.js
@@ -1,82 +1,112 @@
 var historial = [];
 var last_resulto;
 let last_op;  // ultima opcion seleccionada
+let total_point = 0;
 
 const action = operacion => {
     let num1 = document.getElementById("entrada").value;
     let op = operacion.value;
-
+    //    last_op =  num1.slice(-1);
     switch (op) {
         case "+":
-            if (last_op != '.' && last_op != '+' && last_op != '-' && last_op != '*' && last_op != '/') {
+            if (num1.slice(-1) != '.' && last_op != '+' && last_op != '-' && last_op != '*' && last_op != '/') {
                 document.getElementById('entrada').value += '+';
                 last_op = "+";
+                total_point = countpoint();
+
             }
             break;
         case "-":
-            if (last_op != '.' && last_op != '+' && last_op != '-' && last_op != '*' && last_op != '/') {
+            if (num1.slice(-1) != '.' && last_op != '+' && last_op != '-' && last_op != '*' && last_op != '/') {
                 document.getElementById('entrada').value += '-';
                 last_op = "-";
+                total_point = countpoint();
+
             }
             break;
         case "*":
-            if (last_op != '.' && last_op != '+' && last_op != '-' && last_op != '*' && last_op != '/') {
+            if (num1.slice(-1) != '.' && last_op != '+' && last_op != '-' && last_op != '*' && last_op != '/') {
                 document.getElementById('entrada').value += '*';
                 last_op = "*";
+                total_point = countpoint();
+
             }
             break;
         case "/":
-            if (last_op != '.' && last_op != '+' && last_op != '-' && last_op != '*' && last_op != '/') {
+            if (num1.slice(-1) != '.' && last_op != '+' && last_op != '-' && last_op != '*' && last_op != '/') {
                 document.getElementById('entrada').value += '/';
                 last_op = "/";
+                total_point = countpoint();
+
             }
             break;
         case ".":
-            if (last_op != '.' && last_op != '+' && last_op != '-' && last_op != '*' && last_op != '/') {
+
+            if (total_point <= 1 && last_op != '.' && last_op != '+' && last_op != '-' && last_op != '*' && last_op != '/') {
                 document.getElementById('entrada').value += '.';
                 last_op = ".";
+                console.log(countpoint());
             }
             break;
         case "1":
             document.getElementById('entrada').value += '1';
             last_op = null;
+            total_point = countpoint();
+
             break;
         case "2":
             document.getElementById('entrada').value += '2';
             last_op = null;
+            total_point = countpoint();
+
             break;
         case "3":
             document.getElementById('entrada').value += '3';
             last_op = null;
+            total_point = countpoint();
+
             break;
         case "4":
             document.getElementById('entrada').value += '4';
             last_op = null;
+            total_point = countpoint();
+
             break;
         case "5":
             document.getElementById('entrada').value += '5';
             last_op = null;
-            break;
+            total_point = countpoint();
 
+            break;
         case "6":
             document.getElementById('entrada').value += '6';
             last_op = null;
+            total_point = countpoint();
+
             break;
         case "7":
             document.getElementById('entrada').value += '7';
             last_op = null;
+            total_point = countpoint();
+
             break;
         case "8":
             document.getElementById('entrada').value += '8';
             last_op = null;
+            total_point = countpoint();
+
             break;
         case "9":
             document.getElementById('entrada').value += '9';
             last_op = null;
+            total_point = countpoint();
+
             break;
         case "0":
             document.getElementById('entrada').value += '0';
             last_op = null;
+            total_point = countpoint();
+
             break;
 
     }
@@ -106,7 +136,12 @@ function result() {
             last_op = null;
 
             document.getElementById('entrada').value = result_op;
+            console.log(countpoint());
+            total_point = countpoint();
 
+            if (total_point == 1) {
+                last_op = '.'
+            }
 
         }
     }
@@ -136,9 +171,25 @@ function limpiar_historial() {
 function retry() {
     let str = document.getElementById('entrada').value;
     const str2 = str.substring(0, str.length - 1);
-    if(str.substr(-1) == '+' || str.substr(-1) == '-' || str.substr(-1) == '*' || str.substr(-1) == '/' || str.substr(-1) == '.'){
+    if (str.substr(-1) == '+' || str.substr(-1) == '-' || str.substr(-1) == '*' || str.substr(-1) == '/' || str.substr(-1) == '.') {
         last_op = null;
     }
     document.getElementById('entrada').value = str2;
+
+}
+
+
+const countpoint = function () {
+    count = 0;
+    field = document.getElementById("entrada").value;
+    for (index = 0; index < field.length; index++) {
+        if (field[index] == '.') {
+            count++
+        }
+    }
+    return count;
+    //field.slice(-1) == '.'
+    //document.getElementById('entrada').value = act.substring(0, act.length - 1);
+
 
 }
